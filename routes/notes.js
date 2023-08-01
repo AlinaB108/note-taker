6b9e7f4efa0c46948f87425f26a1378585e32f93
Alina Badillo <alinachristabel108@gmail.com>
2023-08-01 15:05:47 -0500

    Changed database file name to notes.json and added GET route into notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -6,4 +6,23 @@ const {
   writeToFile,
 } = require('../helpers/fsUtils');
 
+// GET Route for retrieving notes
+notes.get('/', (req, res) => {
+  readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// GET Route for a specific note
+notes.get('/:note_id', (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile('./db/notes.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.note_id === noteId);
+      return result.length > 0
+        ? res.json(result)
+        : res.json('No note with that ID');
+    });
+});
+
+
 module.exports = notes;
